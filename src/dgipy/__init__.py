d97e0ee80dccc8791f97358be49d63838a09cb45
mcannon068nw <Matthew.Cannon2@nationwidechildrens.org>
2024-01-31 12:36:58 -0500

    fix repo init things

--- src/dgipy/__init__.py
+++ src/dgipy/__init__.py
@@ -1,5 +1,6 @@
 """Python wrapper for accessing an instance of DGIdb v5 database"""
-from .dgidb import dgidb, graph_app, network_graph
+from .dgidb import get_drug, get_gene, get_interactions, get_categories, get_source, get_gene_list, get_drug_applications
+from .graph_app import generate_app
 
 __all__ = [
     get_drug,
