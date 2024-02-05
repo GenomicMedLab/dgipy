94c82a1b9515a6ddb1308c347508ad9e226ed37e
rbasu101 <Rohit.Basu@nationwidechildrens.org>
2024-02-05 16:53:07 -0500

    pyproject.toml:  -Added version (Seems to fix an error in initiating environment)  -Added two necessary dependent packages (Something to look into later) pytest:  -Readded test files to project
     Note:
        Currently, there are import errors involving the test files (To Fix)

---
+++ tests/test_dgidb.py
@@ -0,0 +1,92 @@
+from dgipy.dgidb import get_interactions,get_categories,get_drug
+import requests
+import pandas as pd
+import pytest
+
+def test_get_interactions():
+    """Test that interactions works correctly"""
+
+    # Search types work correctly
+    query = 'braf'
+    with pytest.raises(Exception) as excinfo:
+        results = get_interactions(query,search='fake')
+    assert "Search type must be specified" in str(excinfo.value)
+
+    # Default search type functions correct as 'gene' and not 'drug'
+    query = 'imatinib'
+    results = get_interactions(query)
+    assert len(results) == 0
+
+    # Use pandas default is correctly set to 'true'
+    query = 'braf'
+    results = get_interactions(query)
+    assert type(results) == type(pd.DataFrame())
+
+    # Use pandas can be toggled to 'false' and returns a dictionary response object
+    query = 'braf'
+    results = get_interactions(query,use_pandas=False)
+    assert type(results) != type(pd.DataFrame())
+    assert type(results) == type(dict())
+
+    # Gene search types work correctly
+    query = 'braf'
+    results = get_interactions(query,search='genes')
+    assert results.columns[0] == 'gene'
+
+    # Gene search is not grabbing drugs
+    query = 'imatinib'
+    results = get_interactions(query,search='genes')
+    assert len(results) == 0
+
+    # Drug search types work correctly
+    query = 'imatinib'
+    results = get_interactions(query,search='drugs')
+    assert type(results) == type(pd.DataFrame())
+    assert results.columns[0] == 'drug'
+
+    # Drug search is not grabbing genes
+    query = 'braf'
+    results = get_interactions(query,search='drugs')
+    assert len(results) == 0
+
+    pass
+
+def test_get_categories():
+    """Test that categories works correctly"""
+
+    # Categories search builds a dataframe (with use_pandas default set to 'true')
+    query = 'braf'
+    results = get_categories(query)
+    assert type(results) == type(pd.DataFrame())
+
+    # Categories does not return drugs data
+    query = 'imatinib'
+    results = get_categories(query)
+    assert len(results) == 0
+
+    # Use pandas can be toggled to 'false' and returns a dictionary response object
+    query = 'imatinib'
+    results = get_categories(query,use_pandas=False)
+    assert type(results) != type(pd.DataFrame())
+    assert type(results) == type(dict())
+
+    pass
+
+def test_get_drugs():
+    """Test that drug profile works correctly"""
+
+    # Drug search builds a dataframe (with use_pandas default set to 'true')
+    query = 'imatinib'
+    results = get_drug(query)
+    assert type(results) == type(pd.DataFrame())
+
+    # Drug search does not return gene data
+    query = 'braf'
+    results = get_drug(query)
+    assert(len(results)) == 0
+
+    # Use pandas can be toggled to 'false' and returns dictionary response object
+    query = 'imatinib'
+    results = get_drug(query,use_pandas=False)
+    assert type(results) != type(pd.DataFrame())
+    assert type(results) == type(dict())
