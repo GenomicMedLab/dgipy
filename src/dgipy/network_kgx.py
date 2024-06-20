de9257106e64e9a7d74bb8aa5e289928695c902a
Rohit Basu <Rohit.Basu@nationwidechildrens.org>
2024-06-20 12:47:41 -0400

    Made merging writeup, added writing functionality
    biolink-kgx.ipynb:
        Removed previous contents (as it has now been implemented in network_kgx.py)
        Now has a mini-writeup on an attempted network_graph.py and network_kgx.py merge
    
    network_kgx.py:
        Added 'writing to file' functionality.
        Makes advantage of kgx's ability to convert to various data formats, such as json and tsv
        TODO: Add reading functionality
        TODO: Add neo4j writing functionality

--- src/dgipy/network_kgx.py
+++ src/dgipy/network_kgx.py
@@ -1,9 +1,14 @@
+import os
+
 import pandas as pd
 from kgx.graph.nx_graph import NxGraph
+from kgx.sink import JsonSink, TsvSink
+from kgx.transformer import Transformer
 from kgx.validator import Validator
 
 from dgipy import dgidb
 
+TARGET_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), "target")
 
 def get_kgx_network_graph(terms: list | str, search: str = "genes") -> NxGraph:
     interactions = dgidb.get_interactions(terms, search)
@@ -39,6 +44,31 @@ def __fix_conceptid_prefix(concept_id: str) -> str:
         return prefix_rules[prefix] + ":" + suffix
     return concept_id
 
+
+def generate_kgx_json(graph: NxGraph) -> None:
+    filepath = os.path.join(TARGET_DIR, "graph.json")
+    t = Transformer()
+    s = JsonSink(t, filename = filepath)
+    for n, data in graph.nodes(data=True):
+        s.write_node(data)
+    for u, v, k, data in graph.edges(data=True, keys=True):
+        s.write_edge(data)
+    s.finalize()
+
+def generate_kgx_tsv(graph: NxGraph) -> None:
+    t = Transformer()
+    s = TsvSink(
+        owner = t,
+        filename = os.path.join(TARGET_DIR, "graph"),
+        format = "tsv",
+        node_properties = {"id", "name", "category"},
+        edge_properties = {"subject", "predicate", "object", "relation"},
+    )
+    for n, data in graph.nodes(data=True):
+        s.write_node(data)
+    for u, v, k, data in graph.edges(data=True, keys=True):
+        s.write_edge(data)
+    s.finalize()
 
 def __initalize_network(interactions: pd.DataFrame, terms: list, search: str) -> NxGraph:
     interactions_graph = NxGraph()
