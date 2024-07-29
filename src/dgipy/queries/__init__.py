ebffd29e2b849ceb8bf2fae541b69f4113f4064a
James Stevenson <james.stevenson@nationwidechildrens.org>
2024-07-29 15:32:44 -0400

    feat!: restructure query definitions

---
+++ src/dgipy/queries/__init__.py
@@ -0,0 +1,46 @@
+"""Provide queries and lazy accessors to queries.
+
+
+Individual query loader classes provide ``gql`` queries via a ``.query`` property:
+
+>>> from dgipy.queries import get_drugs
+>>> get_drugs.query
+DocumentNode at 0:545
+"""
+from importlib import resources
+from gql import gql
+
+
+class _LazyQueryLoader:
+
+    def __init__(self, query_name: str):
+        self.query_name = query_name
+        self._query = None
+
+    @property
+    def query(self):
+        if self._query is None:
+            with resources.open_text(__name__, f"{self.query_name}.graphql") as f:
+                self._query = gql(f.read())
+        return self._query
+
+get_all_genes = _LazyQueryLoader("get_all_genes")
+get_drug_applications = _LazyQueryLoader("get_drug_applications")
+get_drugs = _LazyQueryLoader("get_drugs")
+get_gene_categories = _LazyQueryLoader("get_gene_categories")
+get_genes = _LazyQueryLoader("get_genes")
+get_interactions_by_drug = _LazyQueryLoader("get_interactions_by_drug")
+get_interactions_by_gene = _LazyQueryLoader("get_interactions_by_gene")
+get_sources = _LazyQueryLoader("get_sources")
+
+
+__all__ = [
+    "get_all_genes",
+    "get_drug_applications",
+    "get_drugs",
+    "get_gene_categories",
+    "get_genes",
+    "get_interactions_by_drug",
+    "get_interactions_by_gene",
+    "get_sources",
+]
