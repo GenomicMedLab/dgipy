9fa76eea99be1d671681c84151a204f3b496cb23
James Stevenson <james.stevenson@nationwidechildrens.org>
2024-08-28 21:14:51 -0400

    Add TSV util

--- src/dgipy/type_utils.py
+++ src/dgipy/type_utils.py
@@ -1,5 +1,8 @@
 """Provide utilities relating to data types."""
 
+import csv
+from pathlib import Path
+
 
 def make_tabular(columnar_dict: dict) -> list[dict]:
     """Convert DGIpy query method output to a tabular format.
@@ -12,3 +15,16 @@ def make_tabular(columnar_dict: dict) -> list[dict]:
         dict(zip(columnar_dict.keys(), row, strict=False))
         for row in zip(*columnar_dict.values(), strict=False)
     ]
+
+
+def dump_columnar_to_tsv(columnar_dict: dict, output_file: Path) -> None:
+    """Dump DGIpy query method output to a TSV file.
+
+    :param columnar_dict: column-oriented dict as returned by DGIpy query methods
+    :param output_file: path to save location
+    """
+    rows = zip(*columnar_dict.values(), strict=False)
+    with output_file.open("w", newline="") as tsvfile:
+        writer = csv.writer(tsvfile, delimiter="\t")
+        writer.writerow(columnar_dict.keys())
+        writer.writerows(rows)
